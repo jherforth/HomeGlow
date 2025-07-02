e8350cfb53410a115baec27e8074f079efd15711
Jason Herforth <herforth.jason@gmail.com>
2025-07-02 14:55:11 -0400

    update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Import your main App component
-import './index.css'; // Import your global CSS
+import App from './app.js'; // Corrected import path
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
