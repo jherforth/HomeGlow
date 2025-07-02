33de45263f0b2aebc4c1b2b1873bc298feab6198
Jason Herforth <herforth.jason@gmail.com>
2025-07-02 14:59:34 -0400

    updated from js to jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.js'; // Corrected import path
+import App from './app.jsx'; // Corrected import path
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
