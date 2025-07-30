9941a519ff2b2d61104857e12fb96a522b380713
jherforth <herforth.jason@gmail.com>
2025-07-30 11:08:44 -0400

    Updated ChoreWidget.jsx

--- client/src/components/ChoreWidget.jsx
+++ client/src/components/ChoreWidget.jsx
@@ -581,6 +581,7 @@ const ChoreWidget = ({ transparentBackground }) => {
           </Box>
         </Box>
       </Box>
+      </Box>
 
       {/* Add Chore Dialog */}
       <Dialog open={showAddDialog} onClose={() => setShowAddDialog(false)} maxWidth="sm" fullWidth>
