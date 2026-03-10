cbb468271ea2bbda8db0528aeabb68c7e67cb6d9
Jay <herforth.jason@gmail.com>
2026-03-10 13:56:44 -0400

    Updated apiConfig.js

--- client/src/utils/apiConfig.js
+++ client/src/utils/apiConfig.js
@@ -2,7 +2,7 @@ export const getApiUrl = () => {
   if (import.meta.env.MODE === 'development') {
     return import.meta.env.VITE_REACT_APP_API_URL || 'http://localhost:5001';
   }
-  return '';
+  return import.meta.env.VITE_REACT_APP_API_URL || '';
 };
 
 export const API_BASE_URL = getApiUrl();
