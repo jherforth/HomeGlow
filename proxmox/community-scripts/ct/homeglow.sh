d0cfe4cab30c9ed88e2ab59367ae52c259baf557
Jay <herforth.jason@gmail.com>
2026-08-18 08:40:34 -0400

    Update homeglow.sh

--- proxmox/community-scripts/ct/homeglow.sh
+++ proxmox/community-scripts/ct/homeglow.sh
@@ -2,7 +2,7 @@
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2026 community-scripts ORG
 # Author: jherforth
-# License: AGPLv3 | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/jherforth/HomeGlow
 
 APP="HomeGlow"
