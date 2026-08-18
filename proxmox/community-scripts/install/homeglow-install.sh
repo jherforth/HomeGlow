24c1a0d0c9e75a0d3ef5134a33fae0ca7a2e3af1
Jay <herforth.jason@gmail.com>
2026-08-18 08:37:30 -0400

    Update copyright and license information in script

--- proxmox/community-scripts/install/homeglow-install.sh
+++ proxmox/community-scripts/install/homeglow-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# Copyright (c) 2021-2025 community-scripts ORG
+# Copyright (c) 2026 community-scripts ORG
 # Author: jherforth
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: APGLv3 | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/jherforth/HomeGlow
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
